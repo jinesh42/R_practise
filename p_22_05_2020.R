6efc502912e1419c3275eaec47b1daf73d7292f1
jinesh42 <jain.jinesh220@gmail.com>
2020-05-25 01:59:33 +0530

    Added Name and age user input and created list of it

--- p_22_05_2020.R
+++ p_22_05_2020.R
@@ -1,3 +1,10 @@
-result <- as.numeric(readline(prompt = "Enter your Birth Year?"))
-age<-2020-result
-cat("Your age is ",age)
+result <- as.numeric(readline(prompt = "Enter your Birth Year?")) #Taking Input from User
+age<-2020-result  # Calculating age
+name=as.character(readline(prompt= "Please give your name")) #Taking Input from User
+cat("Your name is",name,"\n") 
+cat("Your age is ",age,'\n')
+res=list(name,age)   
+print(paste("Result are being stored in list",res[1][1],",",res[2][1]))
+# list cannot be print in cat function as it is only used for atomic vectors.
+
+
