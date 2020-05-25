db3f34315110f525bf7acb1594e75ec88d1b466c
jinesh42 <jain.jinesh220@gmail.com>
2020-05-26 01:27:41 +0530

    Adding matrix operation script

---
+++ matrix.r
@@ -0,0 +1,12 @@
+# matrix
+
+x=matrix(1:3,nrow=3,,ncol=3,byrow=TRUE) # there are represented as colum wise
+p=cbind(5:7,9:11,15:17)
+z1=x+p
+z2=x*p
+z2
+
+
+
+
+
--- p_22_05_2020.R
+++ p_22_05_2020.R
@@ -4,6 +4,7 @@ name=as.character(readline(prompt= "Please give your name")) #Taking Input from 
 cat("Your name is",name,"\n") 
 cat("Your age is ",age,'\n')
 res=list(name,age)   
+names(res)=c("Name","Age") # Added Names for elements of list
 print(paste("Result are being stored in list",res[1][1],",",res[2][1]))
 # list cannot be print in cat function as it is only used for atomic vectors.
 
